08cdd7bcbe6ec60c2d6d545ea48b7e1fe71078ab
codesabersdev <159543459+codesabersdev@users.noreply.github.com>
2024-03-09 00:58:09 +0400

    Refinement to backend, main, and added images for sky conditions.

--- backend.py
+++ backend.py
@@ -2,20 +2,22 @@ import requests
 API_KEY = ""
 
 
-def get_data(city, forecast_days=None, data_type=None):
+def get_data(city, forecast_days, option):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}"
     response = requests.get(url)
     raw_data = response.json()
     filtered_data = raw_data["list"]
     nr_value = 8*forecast_days
     filtered_data = filtered_data[:nr_value]
-    match data_type:
+    dates = [data["dt_txt"] for data in filtered_data]
+    match option:
         case "Temperature":
-            filtered_data = [data["main"]["temp"] for data in filtered_data]
+            filtered_data = [float(data["main"]["temp"]) / 10 for data in filtered_data]
         case "Sky Conditions":
             filtered_data = [data["weather"][0]["main"] for data in filtered_data]
-    return filtered_data
+
+    return filtered_data, dates
 
 
 if __name__ == "__main__":
-    print(get_data(city="Sharjah", forecast_days=3, data_type="Sky Conditions"))
+    print(get_data(city="Sharjah", forecast_days=3, option="Temperature"))
--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
-
+# Add title, text input, slider and select-box
 st.title("Weather Forecast for the Next Days")
-
 place = st.text_input("City Name")
 days = st.slider(label="Days", min_value=1, max_value=5, value=1,
                  help="Select the number of days to see the forecast")
 option = st.selectbox('Select the data to show', ('Temperature', 'Sky Conditions'))
-st.subheader(f"{option} for next {days} days for {place}")
+st.subheader(f"{option} for next {days} days in {place}")
 
+# if condition to check if the user has given any input
+if place:
+    # If the city name is misspelled
+    try:
+        # Get temperature/sky conditions data
+        data, dates = get_data(city=place, forecast_days=days, option=option)
 
-def get_data(slider_days):
-    dates = ["2024-03-01", "2024-03-02", "2024-03-03"]
-    temperature = [10, 20, 5]
-    temperature = [slider_days * temp for temp in temperature]
-    return dates, temperature
-
-
-d, t = get_data(days)
-figure = px.line(x=d, y=t, labels={"x": "Dates", "y": "Temperature(c)"})
-st.plotly_chart(figure)
+        match option:
+            case "Temperature":
+                figure = px.line(x=dates, y=data, labels={"x": "Dates", "y": "Temperature(c)"})
+                st.plotly_chart(figure)
+            case "Sky Conditions":
+                images = [f"./images/{sky.lower()}.png" for sky in data]
+                st.image(images, width=100, caption=dates)
+    except KeyError:
+        st.warning("City doesn't exist. Check the entered city name!")
